58dbed969fc1ee178bb9186f06dbb033ff48e8d4
khalilovanigar <khalilovanigar64@gmail.com>
2025-07-06 01:26:34 +0400

    Update doctormanager.cs

--- Project final/Models/doctormanager.cs
+++ Project final/Models/doctormanager.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace DoctorManagerNamespace;
 
-namespace Project_final.Models
-{
-    class doctormanager
+using System;
+
+    public class DoctorManager
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int ExperienceYears { get; set; }
+        public string Department { get; set; }
+        public string Description { get; set; }
+
+        public DoctorManager(string name, string surname, int experienceYears, string department, string description)
+        {
+            Name = name;
+            Surname = surname;
+            ExperienceYears = experienceYears;
+            Department = department;
+            Description = description;
+        }
+
+        public void ShowCV()
+        {
+            Console.WriteLine("Doctor CV:");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Surname: {Surname}");
+            Console.WriteLine($"Experience year: {ExperienceYears} il");
+            Console.WriteLine($"Departament: {Department}");
+            Console.WriteLine($"About: {Description}");
+        }
     }
-}
+
