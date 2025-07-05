08c234e7c955dc10889b11ae7422d69109eb261b
khalilovanigar <khalilovanigar64@gmail.com>
2025-07-06 01:25:26 +0400

    Update usermanager.cs

--- Project final/Models/usermanager.cs
+++ Project final/Models/usermanager.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Project_final.Models
+namespace MenuHelperNamespace
 {
-    class usermanager
+    using System;
+    using System.Collections.Generic;
+
+    public static class MenuHelper
     {
+        public static int SelectFromMenu(List<string> options, string title)
+        {
+            int selectedIndex = 0;
+            ConsoleKey key;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"--- {title} ---");
+                for (int i = 0; i < options.Count; i++)
+                {
+                    if (i == selectedIndex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"> {options[i]}");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"  {options[i]}");
+                    }
+                }
+
+                var keyInfo = Console.ReadKey(true);
+                key = keyInfo.Key;
+
+                if (key == ConsoleKey.UpArrow && selectedIndex > 0)
+                    selectedIndex--;
+                else if (key == ConsoleKey.DownArrow && selectedIndex < options.Count - 1)
+                    selectedIndex++;
+
+            } while (key != ConsoleKey.Enter);
+
+            return selectedIndex;
+        }
     }
 }
