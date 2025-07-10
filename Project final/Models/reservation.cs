60a6b0aa4211265885c24e159f75419cfb845ce1
khalilovanigar <khalilovanigar64@gmail.com>
2025-07-10 17:36:11 +0400

    Update reservation.cs

--- Project final/Models/reservation.cs
+++ Project final/Models/reservation.cs
@@ -1,99 +1,30 @@
 using System;
 using UsersNamespace;
-namespace ReservNamespace;
 using DoctorManagerNamespace;
 using DoctorsNamespace;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace ReservNamespace;
 
 public class Reserv
 {
     public string Time { get; set; }
     public bool IsReserved { get; set; }
+    [JsonIgnore] 
     public User? ReservedBy { get; set; }
+
+    public Reserv() { }
 
     public Reserv(string time)
     {
         Time = time;
         IsReserved = false;
+        ReservedBy = null;
     }
+    
+
+    
 }
 
-
-
-
-
-/*
-
-
-    else if (choice == 4)
-    {
-        Console.Clear();
-        System.Console.WriteLine("                   ------- Doctor Sign Up --------");
-        System.Console.WriteLine();
-
-        System.Console.Write("Enter your Name: ");
-        string doctorName = Console.ReadLine()!;
-
-        System.Console.Write("Enter your Surname: ");
-        string doctorSurname = Console.ReadLine()!;
-
-        System.Console.Write("Enter your Experience (years): ");
-        bool isParsed = int.TryParse(Console.ReadLine(), out int experience);
-        if (!isParsed)
-        {
-            System.Console.WriteLine("Invalid input. Please enter a number for experience.");
-            Console.ReadKey();
-            return;
-        }
-
-
-        System.Console.WriteLine("Select Department:");
-        System.Console.WriteLine("1. Pediatriya");
-        System.Console.WriteLine("2. Travmatologiya");
-        System.Console.WriteLine("3. Stamotologiya");
-
-        int deptChoice;
-        while (!int.TryParse(Console.ReadLine(), out deptChoice) || deptChoice < 1 || deptChoice > 3)
-        {
-            System.Console.WriteLine("Invalid input. Enter a number");
-        }
-
-        string department = "";
-
-        if (deptChoice == 1)
-        {
-            department = "Pediatriya";
-        }
-        else if (deptChoice == 2)
-        {
-            department = "Travmatologiya";
-        }
-        else if (deptChoice == 3)
-        {
-            department = "Stamotologiya";
-        }
-        else
-        {
-            System.Console.WriteLine("This choice does not exist");
-        }
-
-        Console.Write("Enter a short description about yourself: ");
-        string description = Console.ReadLine()!;
-
-        Doctor newDoctor = new Doctor(doctorName, doctorSurname, experience, department, description);
-
-        string filePath = "doctors_data.json";
-
-        List<Doctor> doctors = Doctor.LoadDoctorsFromJson(filePath);
-
-        doctors.Add(newDoctor);
-        //Admin admin = new Admin();
-        admin.AddDoctorApplication(newDoctor);
-
-        Doctor.SaveDoctorsToJson(filePath, doctors);
-
-        System.Console.WriteLine();
-        System.Console.WriteLine($"Thank you Dr. {doctorName} {doctorSurname}, your application has been submitted for review.You will be notified once admin reviews your application/n");
-        System.Console.WriteLine("Press any key to return to the main menu...");
-        Console.ReadKey();
-    }
-*/
